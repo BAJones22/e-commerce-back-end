457d12b7bb7153c433887cf627c0334e8827f584
Brooke Adlesperger-Jones <badles@live.com>
2023-06-15 18:05:09 -0500

    updated router.get in category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,25 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    try {
+        const categories = await Category.findAll({ include: [Product] });
+        res.status(200).json(categories);
+    } catch (err) {
+        res.status(500).json({ message: 'Error occurred'});
+    }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    try {
+        const category = await Category.findByPk(req.params.id, { include: [Product] });
+        if (!category) {
+            res.status(404).json({ message: 'Error occurred' });
+            return;
+        }
+        res.status(200).json(category);
+    }   catch (err) {
+        res.status(500).json({ message: 'Error occurred'});
+    }
 });
 
 router.post('/', (req, res) => {
