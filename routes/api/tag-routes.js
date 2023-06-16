5d096442fa1ea0aa3005c905737bda9a734402e7
Brooke Adlesperger-Jones <badles@live.com>
2023-06-16 01:37:51 -0500

    updated tag-routes.js file

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,63 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const express = require('express');
+const router = express.Router();
+const { Tag, Product } = require('../../models');
 
-// The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-});
-
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-});
-
-router.post('/', (req, res) => {
-  // create a new tag
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
-
-module.exports = router;
+router.get('/', async (req, res) => {
+    try {
+      const tags = await Tag.findAll({ include: [Product] });
+      res.status(200).json(tags);
+    } catch (err) {
+      res.status(500).json({ message: 'Error occurred' });
+    }
+  });
+  
+  router.get('/:id', async (req, res) => {
+    try {
+      const tag = await Tag.findByPk(req.params.id, { include: [Product] });
+      if (!tag) {
+        res.status(404).json({ message: 'Error occurred' });
+        return;
+      }
+      res.status(200).json(tag);
+    } catch (err) {
+      res.status(500).json({ message: 'Error occurred' });
+    }
+  });
+  
+  router.post('/', async (req, res) => {
+    try {
+      const newTag = await Tag.create(req.body);
+      res.status(200).json(newTag);
+    } catch (err) {
+      res.status(400).json({ message: 'Error occurred' });
+    }
+  });
+  
+  router.put('/:id', async (req, res) => {
+    try {
+      const updated = await Tag.update(req.body, { where: { id: req.params.id } });
+      if (!updated[0]) {
+        res.status(404).json({ message: 'Error occurred' });
+      } else {
+        res.status(200).json(updated);
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Error occurred' });
+    }
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deleted = await Tag.destroy({ where: { id: req.params.id } });
+      if (!deleted) {
+        res.status(404).json({ message: 'Error occurred' });
+      } else {
+        res.status(200).json(deleted);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
