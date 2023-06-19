da7033e9290ef80506ab76b776a21baf5fb4654f
Brooke Adlesperger-Jones <badles@live.com>
2023-06-19 17:54:22 -0500

    updated delete function in product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,13 +36,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
-  try {
-    const newProduct = await Product.create(req.body);
-    res.status(200).json(newProduct);
-  } catch (err) {
-    res.status(400).json({ message: 'Error occurred' });
-  }
+// create new product
+router.post('/', (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
 
   Product.create(req.body)
     .then((product) => {
@@ -111,8 +114,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Product.destroy({ where: { id: req.params.id } });
+    if (!deleted) {
+      res.status(404).json({ message: 'Error occurred' });
+    } else {
+      res.status(200).json(deleted);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
