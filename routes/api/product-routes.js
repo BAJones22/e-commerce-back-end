b9ed8d4deab0f75bfd01025277a4c4423a9ef804
Brooke Adlesperger-Jones <badles@live.com>
2023-06-19 17:26:01 -0500

    updated get route in product-routes.js file'

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -12,12 +12,12 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const products = await Product.findByPk(req.params.id, { include: [Tag, Category] });
-    if (!products) {
+    const product = await Product.findByPk(req.params.id, { include: [Tag, Category] });
+    if (!product) {
       res.status(404).json({ message: 'Error occurred' });
       return;
     }
-    res.status(200).json(products);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json({ message: 'Error occurred' });
   }
