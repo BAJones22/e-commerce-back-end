2489d61c139bc495e50918d4b7bcbbc1e0145e78
Brooke Adlesperger-Jones <badles@live.com>
2023-06-19 17:13:13 -0500

    updated router.post function in product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,16 +36,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post('/', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(400).json({ message: 'Error occurred' });
+  }
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
